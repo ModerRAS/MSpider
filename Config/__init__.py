53dcb65946abb5b0b054f0508e6535ffb5ec51a5
ModerRAS <adqewsx@gmail.com>
2017-09-01 20:45:48 +0800

    添加了一些定义

---
+++ Config/__init__.py
@@ -0,0 +1,2 @@
+ServerHost = "tcp://127.0.0.1"
+ServerPort = "5555"
--- Controller/ControlCenter.py
+++ Controller/ControlCenter.py
@@ -1,1 +1,19 @@
+import json
+
 import zmq
+from multiprocessing import Process
+
+from Config import *
+
+
+class ControlCenter(Process):
+    def __init__(self):
+        Process.__init__(self)
+        self.__name__ = "ControlCenter"
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind("tcp://0.0.0.0:" + str(ServerPort))
+        self.queue = {}
+
+    def run(self):
+        recv = json.loads(self.socket.recv_json())
