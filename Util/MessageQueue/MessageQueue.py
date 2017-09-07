a8e000ea8796fdd47e23ef4a5c8b6186a23af47b
ModerRAS <adqewsx@gmail.com>
2017-09-07 10:13:21 +0800

    AutoSave --AutoGit

--- Util/MessageQueue/MessageQueue.py
+++ Util/MessageQueue/MessageQueue.py
@@ -1,3 +1,21 @@
-class MessageQueue:
-    def __init__(self):
-        pass
+import json
+
+import zmq
+from multiprocessing import Process
+
+
+class MessageQueue(Process):
+    def __init__(self, InsideCode, ServerPort: int = 5555, name: str = "MessageQueue"):
+        Process.__init__(self)
+        self.__name__ = name
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REP)
+        self.socket.bind("tcp://0.0.0.0:" + str(ServerPort))
+        self.queue = {}
+        self.InsideCode = InsideCode
+
+    def run(self):
+        while True:
+            message = json.loads(self.socket.recv_json())
+            self.InsideCode(message)
+            
