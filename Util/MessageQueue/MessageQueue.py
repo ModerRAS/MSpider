d71e193debe4bdfefba0f320fcc406a5f9037d29
ModerRAS <adqewsx@gmail.com>
2017-09-07 11:57:08 +0800

    AutoSave --AutoGit

--- Util/MessageQueue/MessageQueue.py
+++ Util/MessageQueue/MessageQueue.py
@@ -16,12 +16,13 @@ class MessageQueue(Process):
         self.socket.bind("tcp://0.0.0.0:" + str(ServerPort))
         self.queue = {}
         self.InsideCode = InsideCode
-        self.mode = {}
+        self.mode = {1:self.mode_append_queue}
 
     def run(self):
         while True:
             message = json.loads(self.socket.recv_json())
-            id, value = self.InsideCode(message)
+            index, value = self.InsideCode(message)
+            self.mode[index](value)
 
     def mode_append_queue(self,mode:int,li:list):
         try:
@@ -30,7 +31,9 @@ class MessageQueue(Process):
             self.queue[mode] = []
         length = len(self.queue[mode])
         if length >= MaxQueueLength:
-            return False
+            return -1
         else:
             list(map(lambda x:self.queue[mode].append(x),li))
-        
+            return 0
+
+
